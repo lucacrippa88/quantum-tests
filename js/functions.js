c3b01bdbb6cc2a4dbd99f6c2e4bb3b7b3bea3d31
Luca Crippa <luca.crippa88@gmail.com>
2018-07-11 15:23:52 +0200

    Stable for 16 bits emoticon with final )
    So valid for: :) ;) 8) =) and probably etc

--- js/functions.js
+++ js/functions.js
@@ -11,7 +11,14 @@ function stringToBinary(str, spaceSeparatedOctets) {
 
 
 
-function findQGates(bin1, bin2){
+function findQGates(emo1, emo2){
+
+  if ((emo1 == "")||(emo2 == "")) { return; }
+
+  var bin1 = stringToBinary(emo1);
+  var bin2 = stringToBinary(emo2);
+  console.log(emo1+" "+bin1);
+  console.log(emo2+" "+bin2);
 
   // Split binary into arrays
   var arr1 = bin1.split("");
@@ -30,60 +37,73 @@ function findQGates(bin1, bin2){
       }
       // If bits are different, store them and record their position
       if (arr1[i] != arr2[i]) {
-        dif.array1 += arr1[i];
-        dif.array2 += arr2[i];
-        dif.positions += i+" ";
+        dif.array1 += arr1[i]+",";
+        dif.array2 += arr2[i]+",";
+        dif.positions += i+",";
       }
   }
+
+  dif.array1 = dif.array1.substring(0, dif.array1.length - 1);
+  dif.array2 = dif.array2.substring(0, dif.array2.length - 1);
+  dif.positions = dif.positions.substring(0, dif.positions.length - 1);
 
   var dif_str2 = JSON.stringify(dif);
 
   var wtd = "";
 
   // Extract differences positions
-  var pos0 = dif.positions[0];
-  console.log(pos0);
-  var pos1 = dif.positions[1];
-  console.log(pos1);
+  var arr_pos = dif.positions.split(",");
+  var pos0 = arr_pos[0];
+  var pos1 = arr_pos[1];
   // Invert from right to left 0-based
   pos0 = arr1.length-parseInt(pos0)-1;
   pos1 = arr1.length-parseInt(pos1)-1;
 
-
   // Check cases
   if (dif_str2.includes('"array1":"0","array2":"1"')) {
     instructions.push("qc.h(qr["+pos0+"])");
-    wtd = "H sotto";
+    // wtd = "H sotto";
   };
   if (dif_str2.includes('"array1":"1","array2":"0"')) {
     instructions.push("qc.h(qr["+pos0+"])");
-    wtd = "H sopra";
+    // wtd = "H sopra";
   };
-  if (dif_str2.includes('"array1":"11","array2":"00"')) {
+  if (dif_str2.includes('"array1":"1,1","array2":"0,0"')) {
     instructions.push("qc.h(qr["+pos1+"])");
     instructions.push("qc.cx(qr["+pos1+"],qr["+pos0+"])");
-    wtd = "H sotto, CNOT";
+    // wtd = "H sotto, CNOT";
   };
-  if (dif_str2.includes('"array1":"00","array2":"11"')) {
+  if (dif_str2.includes('"array1":"0,0","array2":"1,1"')) {
     instructions.push("qc.h(qr["+pos0+"])");
     instructions.push("qc.cx(qr["+pos0+"],qr["+pos1+"])");
-    wtd = "H sopra, CNOT";
+    // wtd = "H sopra, CNOT";
   };
-  if (dif_str2.includes('"array1":"01","array2":"10"')) {
+  if (dif_str2.includes('"array1":"0,1","array2":"1,0"')) {
     instructions.push("qc.h(qr["+pos0+"])");
     instructions.push("qc.cx(qr["+pos0+"],qr["+pos1+"])");
     instructions.push("qc.x(qr["+pos1+"])");
-    wtd = "H sopra, CNOT, X sotto";
+    // wtd = "H sopra, CNOT, X sotto";
   };
-  if (dif_str2.includes('"array1":"10","array2":"01"')) {
+  if (dif_str2.includes('"array1":"1,0","array2":"0,1"')) {
     instructions.push("qc.h(qr["+pos1+"])");
     instructions.push("qc.cx(qr["+pos1+"],qr["+pos0+"])");
     instructions.push("qc.x(qr["+pos0+"])");
-    wtd = "H sotto, CNOT, X sopra";
+    // wtd = "H sotto, CNOT, X sopra";
   };
 
-  console.log(dif);
-  console.log(wtd);
-  console.log(instructions);
+  // console.log("Differences:");
+  // console.log(dif);
+  // console.log("Logic to apply: "+wtd);
+  // console.log("Quantum Gates:");
+  // console.log(instructions);
+
+  $("#bin-emo1").html(emo1+"&ensp; is &ensp;"+bin1+"<br>");
+  $("#bin-emo2").html(emo2+"&ensp; is &ensp;"+bin2+"<br>");
+
+  for (var k = 0; k < instructions.length; k++) {
+
+    $("#quantized").append(instructions[k]+"<br>");
+
+  }
 
 }
