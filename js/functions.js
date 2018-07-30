ac9676b101cfb3dad5b7603e01078bcdb79f29fd
Luca Crippa <luca.crippa88@gmail.com>
2018-07-30 11:16:52 +0200

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -10,6 +10,70 @@ function stringToBinary(str, spaceSeparatedOctets) {
 };
 
 
+function selectNumber(){
+  $('input[name=radio]').change(function() {
+    if ($("#check2:checked").val()) $("#emo3").css("display", "none");
+    if ($("#check3:checked").val()) $("#emo3").css("display", "block");
+  });
+}
+
+
+function refreshPage(){
+  $("#reset").click(function() {
+    location.reload();
+  });
+}
+
+
+/*
+ * Function: callQ ©
+ * Requires:
+ * Returns:
+ * Author:
+ * Date: July 2018
+ */
+function callQ(){
+
+  $("#go").click(function() {
+
+    // Reset
+    $("#quantized").empty();
+
+    // Prepare binary data
+    var emo1 = stringToBinary($('#emo1').val());
+    var emo2 = stringToBinary($('#emo2').val());
+    var smile_array = [emo1,emo2];
+
+    // Create algorithm frontend
+    findQGates2(emo1,emo2); // used only to check backend algorithm creation and to display it
+
+    // Get run/simulate and set operation
+    if ($("#run:checked").val())
+      var operation = "run";
+    else
+      var operation = "sim";
+
+    console.log(smile_array);
+    console.log(operation);
+
+    $.ajax({
+      url: '', // retrieve page contents
+      type: 'POST',
+      data: {
+        smiles: smile_array,
+        perform: operation
+      },
+      dataType: 'json',
+      success: function(result) {
+
+      } // End of success
+
+    }); // End of ajax call
+
+  });
+
+}
+
 
 
 
@@ -20,12 +84,9 @@ function findQGates2(emo1, emo2){
   // Exit if emoticons aren't found
   if ((emo1 == "")||(emo2 == "")) { return; }
 
-  var bin1 = stringToBinary(emo1);
-  var bin2 = stringToBinary(emo2);
-
   // Define input
-  var arr1 = bin1.split("");
-  var arr2 = bin2.split("");
+  var arr1 = emo1.split("");
+  var arr2 = emo2.split("");
   // Define ancilla structure
   var dif = JSON.parse('{"array1": "","array2": "", "positions": ""}');
   // Define output
@@ -78,8 +139,8 @@ function findQGates2(emo1, emo2){
   }
 
   // Display results
-  $("#bin-emo1").val(bin1);
-  $("#bin-emo2").val(bin2);
+  $("#bin-emo1").val(emo1);
+  $("#bin-emo2").val(emo2);
   $("#bin-emo1").css("display", "block");
   $("#bin-emo2").css("display", "block");
 
@@ -89,88 +150,5 @@ function findQGates2(emo1, emo2){
 
   $("#quantized-card").css("display", "block");
   $("#quantized-result").css("display", "block");
-
-}
-
-
-
-
-// 3 SMILES
-function findQGates3(emo1, emo2, emo3){
-
-  if ((emo1 == "")||(emo2 == "")||(emo3 == "")) { return; }
-
-  var bin1 = stringToBinary(emo1);
-  var bin2 = stringToBinary(emo2);
-  var bin3 = stringToBinary(emo3);
-
-  // Split binary into arrays
-  var arr1 = bin1.split("");
-  var arr2 = bin2.split("");
-  var arr3 = bin3.split("");
-  // Create structure to analyze
-  var dif_str = '{"array1": "","array2": "", "positions": ""}';
-  var dif = JSON.parse(dif_str);
-  // Create array for Q instructions
-  var instructions = [];
-
-  // Loop on binary arrays
-  for (var i = 0; i < arr1.length; i++) {
-      // If bits are equal each other and equal to 1, invert them by adding X gate
-      if ( (arr1[i] == arr2[i])&&(arr1[i] == 1) ){
-        instructions.push("qc.x(qr["+(arr1.length-i-1)+"])");
-      }
-      // If bits are different, store them and record their position
-      if (arr1[i] != arr2[i]) {
-        dif.array1 += arr1[i]+",";
-        dif.array2 += arr2[i]+",";
-        dif.positions += i+",";
-      }
-  }
-
-  dif.array1 = dif.array1.substring(0, dif.array1.length - 1);
-  dif.array2 = dif.array2.substring(0, dif.array2.length - 1);
-  dif.positions = dif.positions.substring(0, dif.positions.length - 1);
-
-  // Extract differences positions
-  var arr_pos = dif.positions.split(",");
-  var arr_emo1 = dif.array1.split(",");
-  var arr_emo2 = dif.array2.split(",");
-  var arr_emo3 = dif.array3.split(",");
-
-  for (var i = 0; i < arr_pos.length; i++) {
-    arr_pos[i] = 16-arr_pos[i]-1;
-    arr_emo1[i] = parseInt(arr_emo1[i]);
-    arr_emo2[i] = parseInt(arr_emo2[i]);
-  }
-
-  console.log(arr_pos);
-  console.log(arr_emo1);
-  console.log(arr_emo2);
-
-  instructions.push("qc.h(qr["+arr_pos[0]+"])");
-
-  for(var i = 1; i < arr_pos.length; i++){
-
-    if (arr_emo1[i] == arr_emo1[0]) {
-      instructions.push("qc.cx(qr["+arr_pos[0]+"],qr["+arr_pos[i]+"])");
-    }
-    else {
-      instructions.push("qc.cx(qr["+arr_pos[0]+"],qr["+arr_pos[i]+"])");
-      instructions.push("qc.x(qr["+arr_pos[i]+"])");
-    }
-
-  }
-
-  console.log(instructions);
-
-  $("#bin-emo1").html(emo1+"&ensp; is &ensp;"+bin1+"<br>");
-  $("#bin-emo2").html(emo2+"&ensp; is &ensp;"+bin2+"<br>");
-
-  for (var k = 0; k < instructions.length; k++) {
-
-    $("#quantized").append(instructions[k]+"<br>");
-
-  }
 
 }
